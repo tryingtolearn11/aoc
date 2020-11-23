8654a834120fa56a2836b7f02cef3ce4a1188765
tryingtolearn11 <damiensingh17@gmail.com>
2020-11-23 14:11:06 -0500

    finished Day 3

--- AOCDAYTHREE/three.py
+++ AOCDAYTHREE/three.py
@@ -1,7 +1,7 @@
 
 # Day 3: Perfectly Spherical Houses in a Vacuum
 
-def find_total_houses(f):
+def find_total_houses_p1(f):
     instructions = f.read()
     houses = set()
     x, y = 0, 0
@@ -32,5 +32,75 @@ def find_total_houses(f):
 
 
 
-find_total_houses(f=open("input.txt","r"))
+# find_total_houses_p1(f=open("input.txt","r"))
 
+# ----------Part Two---------------------------
+
+def find_total_houses_p2(f):
+    instructions = f.read()
+    houses = set()
+    x, y = 0, 0
+    a, b = x, y
+    start = (x, y)
+    # Set Santa's current Position to Start
+    currentSanta = start
+    # Set Robo-Santa's curr pos to start too
+    currentRobo = start 
+    houses.add(start)
+    print("currentSanta : ", currentSanta)
+    print("currentRobo ", currentRobo )
+    
+
+    for i in range(len(instructions)):
+        print("i : ", i)
+        print(instructions[i])
+        if i % 2 == 0: # If Even Give Instructions to Santa
+            if instructions[i] == '^': # Up
+                y-=1
+            elif instructions[i] == 'v': # Down
+                y+=1
+            elif instructions[i] == "<": # Left
+                x-=1
+            else:
+                x+=1
+            currentSanta = (x, y)
+        else: # i is odd -> Give instructions to Robo-Santa
+            if instructions[i] == '^': # Up
+                b-=1
+            elif instructions[i] == 'v': # Down
+                b+=1
+            elif instructions[i] == "<": # Left
+                a-=1
+            else:
+                a+=1
+            currentRobo = (a, b)
+
+        print("Current Santa Position : ", currentSanta)    
+        print("Current Robo- Position : ", currentRobo)
+        houses.add(currentSanta)
+        houses.add(currentRobo)
+    # print(houses)
+    print("Houses visited : ", len(houses))     
+
+
+
+
+
+
+find_total_houses_p2(f=open("input.txt","r"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+find_total_houses_p2(f=open("input.txt","r"))
