bc035870ccfdf23b6f53a98f7dd62eb635fa6ab3
Damien Singh <damiensingh17@gmail.com>
2024-12-12 12:28:04 -0500

    :)

--- 2024/daySeven/daySeven.py
+++ 2024/daySeven/daySeven.py
@@ -4,14 +4,5 @@ def valid(match, res, arr, n, i):
     if n == 0:
         if res ==  match: return True
         else: return False
-    val = arr[i]
-    return valid(match, res + val, arr, n - 1, i + 1) or valid(match, res * val, arr, n - 1, i + 1) or valid(match, int(str(res) + str(val)), arr, n - 1, i + 1)
-
-res = []
-total = 0
-for m, n in equations.items():
-    if valid(m, 0, n, len(n), 0):
-        res.append((m, n))
-        total += m
-print(total)
-    
+    return valid(match, res + arr[i], arr, n - 1, i + 1) or valid(match, res * arr[i], arr, n - 1, i + 1) or valid(match, int(str(res) + str(arr[i])), arr, n - 1, i + 1)
+print(sum([m for m, n in equations.items() if valid(m, 0, n, len(n), 0)]))
